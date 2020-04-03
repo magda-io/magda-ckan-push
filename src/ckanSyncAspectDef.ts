9f4a4827f4d774079fd6b9c59b4b9dc4598be813
Sajid Ibne Anower <sajidanower23@gmail.com>
2020-04-03 13:34:42 +1100

    Copy over ckan-sync schema

--- src/ckanSyncAspectDef.ts
+++ src/ckanSyncAspectDef.ts
@@ -1,5 +1,5 @@
 export default {
     id: "ckan-publisher",
     name: "Ckan Synchronisation Information",
-    jsonSchema: require("@magda/registry-aspects/ckan-sync.schema.json")
+    jsonSchema: require("./ckan-sync.schema.json")
 };
